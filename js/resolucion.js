1ff629200dc62883465bd3290b9aaa60dfaf78fe
GustavoEAybar <aybargustavoe@gmail.com>
2023-08-01 13:27:45 -0300

    solve javascript practical 2

---
+++ js/resolucion.js
@@ -0,0 +1,62 @@
+
+let elections = "c";
+while ( elections.toLowerCase() == 'c'){
+    let options=prompt('Ingrese el punto del practico que quiera probar:');
+    switch(options){
+        case '1':
+            document.write('\n Punto 01');
+            break;
+        case '2':
+            document.write('\n Punto 02');
+            break;
+        case '3':
+            document.write('\n Punto 03');
+            break;
+        case '4':
+            document.write('\n Punto 04');
+            break;
+        case '5':
+            document.write('\n Punto 05');
+            break;
+        case '6':
+            document.write('\n Punto 06');
+            break;
+        case '7':
+            document.write('\n Punto 07');
+            break;
+        case '8':
+            document.write('\n Punto 08');
+            break;
+        case '9':
+            document.write('\n Punto 09');
+            break;
+        case '10':
+            document.write('\n Punto 10');
+            break;
+        case '11':
+            document.write('\n Punto 11');
+            break;
+        case '12':
+            document.write('\n Punto 12');
+            break;
+        case '13':
+            document.write('\n Punto 13');
+            break;
+        case '14':
+            document.write('\n Punto 14');
+            break;
+        case '15':
+            document.write('\n Punto 15');
+            break;
+        case '16':
+            document.write('\n Punto 16');
+            break;
+        case '17':
+            document.write('\n Punto 17');
+            break;
+        default:
+            alert('No es una opcion disponible');
+            break;
+    }
+    elections = prompt('ingresa "s" para salir o "c" para continuar');
+}
