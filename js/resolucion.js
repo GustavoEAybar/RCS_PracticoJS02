72902698878862bb4f35929cbf32d870704a67b8
GustavoEAybar <aybargustavoe@gmail.com>
2023-08-06 01:01:26 -0300

    solve points 1 to 7 of the practical

--- js/resolucion.js
+++ js/resolucion.js
@@ -1,105 +1,3 @@
-/*
-//let options=prompt('Ingrese el punto del practico que quiera probar:');
-//switch(options){
-//    case '1':
-        document.write('\n Punto 01');
-        let edad = prompt('Ingrese su edad: ');
-        if (edad > 18){
-            document.write(`Puede manejar`);
-        }else if (edad < 18){
-            document.write(`No puede manejar`);
-        }else{
-            document.write('este no es un dato valido');
-        }
-//        break;
-//    case '2':
-        document.write('\n Punto 02');
-        let nota = prompt('Ingrese una nota del 0 al 10:');
-        switch(nota){
-            case '0':
-            case '1':
-            case '2':
-                alert('Muy deficiente');
-                break;
-            case '3':
-            case '4':
-                alert('Insuficiente');
-                break;
-            case '5':
-            case '6':
-                alert('Suficiente');
-                break;
-            case '7':
-                alert('Bien');
-                break;
-            case '8':
-            case '9':
-                alert('Notable');
-                break;
-            case '10':
-                alert('Sobresaliente');
-                break;
-            default:
-                if(nota>10||nota<0){
-                    alert('número erróneo')
-                }else{
-                    alert('El dato ingresado no es valido, ingrese un numero dentro del rango (del 0 al 10)');
-                }
-                break;
-        }
-        //        break;
-//    case '3':
-        document.write('\n Punto 03');
-        
-//        break;
-//    case '4':
-        document.write('\n Punto 04');
-//        break;
-//   case '5':
-        document.write('\n Punto 05');
-//        break;
-//    case '6':
-        document.write('\n Punto 06');
-//        break;
-//    case '7':
-        document.write('\n Punto 07');
-//        break;
-//   case '8':
-        document.write('\n Punto 08');
-//        break;
-//    case '9':
-        document.write('\n Punto 09');
-//        break;
-//    case '10':
-        document.write('\n Punto 10');
-//        break;
-//    case '11':
-        document.write('\n Punto 11');
-//        break;
-//    case '12':
-        document.write('\n Punto 12');
-//        break;
-//    case '13':
-        document.write('\n Punto 13');
-//        break;
-//    case '14':
-        document.write('\n Punto 14');
-//        break;
-//    case '15':
-        document.write('\n Punto 15');
-//        break;
-//    case '16':
-        document.write('\n Punto 16');
-//        break;
-//    case '17':
-        document.write('\n Punto 17');
-//        break;
-//    default:
-        alert('No es una opcion disponible');
-//        break;
-//}
-*/
-
 function punto1 (){
 
         let edad = document.getElementsByName("edad")[0].value;
@@ -161,6 +59,7 @@ function punto2(){
                 break;
         }        
 }
+
 function punto3(){
         let cadenaIngresada ="";
         let cadenaTotal = "";
@@ -174,45 +73,164 @@ function punto3(){
         document.getElementsById("respuestaPunto3").innerHTML = cadenaTotal;
 
 }
+
 function punto4(){
-
+        let sumaDeNumeros = 0;
+        let numero4 = 0;
+        do{
+                numero4 = prompt("ingrese un numero: ");
+                if(Number.isNaN(numero4)){
+                        sumaDeNumeros = sumaDeNumeros + numero4;
+                }else{
+                        alert("No es un numero");
+                }
+        }while(confirm('ingresar otro numero?')==true)
+        document.getElementById("respuestaPunto4").innerHTML = `La suma de los numeros ingresados es ${sumaDeNumeros}`;
 }
+
 function punto5(){
-
+        let DNI = 0;
+        //let resultado = 0;
+        do {
+                DNI=prompt("Ingrese un numero de DNI:");
+                if(DNI>=0 && DNI<=99999999){
+                        //resultado = (DNI%23);
+                        switch(DNI%23){
+                                case 0:
+                                        document.getElementById("respuestaPunto5").innerHTML = "El valor de la letra del DNI ingresado es: T";
+                                        break;
+                                case 1:
+                                        document.getElementById("respuestaPunto5").innerHTML = "El valor de la letra del DNI ingresado es: R";
+                                        break;
+                                case 2:
+                                        document.getElementById("respuestaPunto5").innerHTML = "El valor de la letra del DNI ingresado es: W";
+                                        break;
+                                case 3:
+                                        document.getElementById("respuestaPunto5").innerHTML = "El valor de la letra del DNI ingresado es: A";
+                                        break;
+                                case 4:
+                                        document.getElementById("respuestaPunto5").innerHTML = "El valor de la letra del DNI ingresado es: G";
+                                        break;
+                                case 5:
+                                        document.getElementById("respuestaPunto5").innerHTML = "El valor de la letra del DNI ingresado es: M";
+                                        break;
+                                case 6:
+                                        document.getElementById("respuestaPunto5").innerHTML = "El valor de la letra del DNI ingresado es: Y";
+                                        break;
+                                case 7:
+                                        document.getElementById("respuestaPunto5").innerHTML = "El valor de la letra del DNI ingresado es: F";
+                                        break;
+                                case 8:
+                                        document.getElementById("respuestaPunto5").innerHTML = "El valor de la letra del DNI ingresado es: P";
+                                        break;
+                                case 9:
+                                        document.getElementById("respuestaPunto5").innerHTML = "El valor de la letra del DNI ingresado es: D";
+                                        break;
+                                case 10:
+                                        document.getElementById("respuestaPunto5").innerHTML = "El valor de la letra del DNI ingresado es: X";
+                                        break;
+                                case 11:
+                                        document.getElementById("respuestaPunto5").innerHTML = "El valor de la letra del DNI ingresado es: B";
+                                        break;
+                                case 12:
+                                        document.getElementById("respuestaPunto5").innerHTML = "El valor de la letra del DNI ingresado es: N";
+                                        break;
+                                case 13:
+                                        document.getElementById("respuestaPunto5").innerHTML = "El valor de la letra del DNI ingresado es: J";
+                                        break;
+                                case 14:
+                                        document.getElementById("respuestaPunto5").innerHTML = "El valor de la letra del DNI ingresado es: Z";
+                                        break;
+                                case 15:
+                                        document.getElementById("respuestaPunto5").innerHTML = "El valor de la letra del DNI ingresado es: S";
+                                        break;
+                                case 16:
+                                        document.getElementById("respuestaPunto5").innerHTML = "El valor de la letra del DNI ingresado es: Q";
+                                        break;
+                                case 17:
+                                        document.getElementById("respuestaPunto5").innerHTML = "El valor de la letra del DNI ingresado es: V";
+                                        break;
+                                case 18:
+                                        document.getElementById("respuestaPunto5").innerHTML = "El valor de la letra del DNI ingresado es: H";
+                                        break;
+                                case 19:
+                                        document.getElementById("respuestaPunto5").innerHTML = "El valor de la letra del DNI ingresado es: L";
+                                        break;
+                                case 20:
+                                        document.getElementById("respuestaPunto5").innerHTML = "El valor de la letra del DNI ingresado es: C";
+                                        break;
+                                case 21:
+                                        document.getElementById("respuestaPunto5").innerHTML = "El valor de la letra del DNI ingresado es: K";
+                                        break;
+                                case 22:
+                                        document.getElementById("respuestaPunto5").innerHTML = "El valor de la letra del DNI ingresado es: E";
+                                        break;
+                                default:
+                                        break;
+                        }
+                }else{
+                        alert("El dato ingresado no es valido, deve ingresar un DNI entre el 0 y el 99.999.999");
+                }
+        }while(confirm('ingresar otro DNI?')==true);
 }
+
 function punto6(){
-
+        let priIndice=0;
+        for(let segIndice=1; segIndice<31;segIndice++){
+                while(priIndice<segIndice){ 
+                document.getElementById("respuestaPunto6").innerHTML= segIndice;
+                priIndice++;
+                }
+                priIndice=1;
+                //document.getElementById("respuestaPunto6").innerHTML = '<br>';
+        };
 }
+
 function punto7(){
-
+        let tope = prompt("Ingrese un numero:");
+        for(let i=0; i<tope;i++){
+                while(i<=tope){
+                        document.getElementById("respuestaPunto7").innerHTML = tope-i;
+                }
+        };        
 }
+
 function punto8(){
-
+        document.getElementById("respuestaPunto8").innerHTML = "Aun no esta definido...";
 }
+
 function punto9(){
-
+        document.getElementById("respuestaPunto9").innerHTML = "Aun no esta definido...";
 }
+
 function punto10(){
-
+        document.getElementById("respuestaPunto10").innerHTML = "Aun no esta definido...";
 }
+
 function punto11(){
-
+        document.getElementById("respuestaPunto11").innerHTML = "Aun no esta definido...";
 }
+
 function punto12(){
-
+        document.getElementById("respuestaPunto12").innerHTML = "Aun no esta definido...";
 }
+
 function punto13(){
-
+        document.getElementById("respuestaPunto13").innerHTML = "Aun no esta definido...";
 }
+
 function punto14(){
-
+        document.getElementById("respuestaPunto14").innerHTML = "Aun no esta definido...";
 }
+
 function punto15(){
-
+        document.getElementById("respuestaPunto15").innerHTML = "Aun no esta definido...";
 }
+
 function punto16(){
-
+        document.getElementById("respuestaPunto16").innerHTML = "Aun no esta definido...";
 }
-function punto17(){
 
+function punto17(){
+        document.getElementById("respuestaPunto17").innerHTML = "Aun no esta definido...";
 }
